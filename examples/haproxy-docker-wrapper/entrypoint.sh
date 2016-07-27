70c36da14505f6e06ba26e385813e847137978ca
Jaime Soriano Pastor <jsoriano@tuenti.com>
2016-07-27 20:15:14 +0200

    Added example for haproxy-docker-wrapper

---
+++ examples/haproxy-docker-wrapper/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CONFFILE=/usr/local/etc/haproxy/haproxy.cfg
+
+echo "
+global
+	daemon
+
+frontend nothing
+	bind :80
+" > $CONFFILE
+
+sed -i $TEMPLATE \
+	-e "s/__HAPROXY_STATS_BIND__/$HAPROXY_STATS_BIND/" \
+	-e "s/__SYSLOG__/$SYSLOG/"
+
+exec kube2lb -apiserver="$APISERVER" -kubecfg="$KUBECFG" -template="$TEMPLATE" -server-name-templates="$SERVER_NAME_TEMPLATES" -config="$CONFFILE" -domain="$DOMAIN" -notify=command:"curl -s http://$HAPROXY_WRAPPER_CONTROL/reload"
